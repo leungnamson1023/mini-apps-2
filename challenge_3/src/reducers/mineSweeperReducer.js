330952db7ecc42c945ddc0a96329cf6fb836d0a5
Namson Leung <leungnamson@gmail.com>
2018-04-07 11:01:52 -0700

    updated tests as well as reducer func

--- challenge_3/src/reducers/mineSweeperReducer.js
+++ challenge_3/src/reducers/mineSweeperReducer.js
@@ -1,25 +1,54 @@
-import { store } from './store';
 import { INIT_BOARD } from '../actions/actions';
 
-const mineSweeperReducer = (state = store, action = { type: '' }) => {
-  const board = {};
+const defStore = {
+  board: {
+    '0, 0': {
+      hasMine: false,
+      hasFlag: false,
+      isOpen: true,
+    },
+    '0, 1': {
+      hasMine: true,
+      hasFlag: false,
+      isOpen: false,
+    }
+  },
+  size: 9,
+};
+
+const mineSweeperReducer = (state = defStore, action = {type: ""}) => {
   switch (action.type) {
-    case INIT_BOARD:
-      for (let r = 0; r < 9; r += 1) {
-        for (let c = 0; c < 9; c += 1) {
+    case 'INIT_BOARD':
+    const board = {};
+      for (let r = 0; r < action.size; r += 1) {
+        for (let c = 0; c < action.size; c += 1) {
           const location = `${r}, ${c}`;
           board[location] = {};
-          if (action.mineLocations.includes(location)) {
-            board[location].hasMine = true;
-          } else {
-            board[location].hasMine = false;
-          }
+          board[location].hasMine = action.mineLocations.includes(location)
         }
       }
-      return { board, ...state };
+      return { ...state, board };
     default:
       return state;
   }
 };
+
+// const makeBoard = (action = {mineLocations: ['0, 1']}) => {
+//   const board = {};
+//   for (let r = 0; r < 9; r += 1) {
+//     for (let c = 0; c < 9; c += 1) {
+//       const location = `${r}, ${c}`;
+//       board[location] = {};
+//       if (action.mineLocations.includes(location)) {
+//         board[location].hasMine = true;
+//       } else {
+//         board[location].hasMine = false;
+//       }
+//     }
+//   }
+//   return board;
+// }
+
+// console.log(makeBoard());
 
 export default mineSweeperReducer;
--- challenge_3/src/reducers/mineSweeperReducer.test.js
+++ challenge_3/src/reducers/mineSweeperReducer.test.js
@@ -1,16 +1,33 @@
 import mineSweeperReducer from './mineSweeperReducer';
-import store from './store';
 import INIT_BOARD from '../actions/actions';
 
 describe('checks for mines', () => {
   it('init board', () => {
     const action = {
-      type: INIT_BOARD,
-      size: 9,
-      mineLocations: ['0, 1'],
+      type: 'INIT_BOARD',
+      size: 3,
+      mineLocations: ['0, 1', '1, 1']
     };
-    const newBoard = mineSweeperReducer(store, action);
+const defStore = {
+  board: {
+    '0, 0': {
+      hasMine: false,
+      hasFlag: false,
+      isOpen: true,
+    },
+    '0, 1': {
+      hasMine: true,
+      hasFlag: false,
+      isOpen: false,
+    }
+  },
+  size: 9,
+};
+    const newBoard = mineSweeperReducer(defStore, action);
+    console.log(newBoard);
     expect(newBoard.board['0, 0'].hasMine).toEqual(false);
+    expect(newBoard.board['0, 1'].hasMine).toEqual(true);
+
     expect(newBoard.board).toBeDefined();
   });
 });
